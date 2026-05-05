39b4d63d7dd10c72e555cba9942cb1aa2e1e9338
darshanurs <darshanurs54@gmail.com>
2026-05-05 20:02:02 +0530

    Production: Fixed missing CORS requirement in server entry point

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const aiRoutes = require('./routes/ai');
 const emailRoutes = require('./routes/emails');
